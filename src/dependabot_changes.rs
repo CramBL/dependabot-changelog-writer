03cfdedf2476fa67f69ee876279590f8536c887e
Marc Beck König <mbkj@tutamail.com>
2024-12-30 00:44:30 +0100

    run formatter

--- src/dependabot_changes.rs
+++ src/dependabot_changes.rs
@@ -119,7 +119,7 @@ pub fn format_changes(changes: Vec<DependabotChange>) -> String {
 const UPDATE_LINE_KEYWORD_A: &str = "Updates";
 const UPDATE_LINE_KEYWORD_B: &str = "Bumps";
 
-const UPDATE_KEYWORDS: [(& str, usize); 2] = [
+const UPDATE_KEYWORDS: [(&str, usize); 2] = [
     (UPDATE_LINE_KEYWORD_A, UPDATE_LINE_KEYWORD_A.len()),
     (UPDATE_LINE_KEYWORD_B, UPDATE_LINE_KEYWORD_B.len()),
 ];
@@ -161,8 +161,18 @@ mod tests {
     fn test_parse_body() {
         let changes = parse_body(DEPENDABOT_BODY_2_ACTIONS_SHA_SEMVER);
         assert_eq!(changes.len(), 2);
-        assert_eq!(changes[0], DependabotChange::new("`crate-ci/typos`", "1.27.0", "1.28.4"));
-        assert_eq!(changes[1], DependabotChange::new("`docker/login-action`", "3d58c274f17dffee475a5520cbe67f0a882c4dbb", "7ca345011ac4304463197fac0e56eab1bc7e6af0"));
+        assert_eq!(
+            changes[0],
+            DependabotChange::new("`crate-ci/typos`", "1.27.0", "1.28.4")
+        );
+        assert_eq!(
+            changes[1],
+            DependabotChange::new(
+                "`docker/login-action`",
+                "3d58c274f17dffee475a5520cbe67f0a882c4dbb",
+                "7ca345011ac4304463197fac0e56eab1bc7e6af0"
+            )
+        );
         let changes_md = format_changes(changes);
         let expect_md = "\
         - `crate-ci/typos`: 1.27.0 → 1.28.4\n\
@@ -180,15 +190,35 @@ mod tests {
     fn test_parse_example_to_changes_7_crates_semver() {
         let changes = parse_changes(DEPENDABOT_BODY_7_CRATES_SEMVER);
         assert_eq!(changes.len(), 8);
-        assert_eq!(changes[0], DependabotChange::new("`serde`", "1.0.215", "1.0.216"));
-        assert_eq!(changes[1], DependabotChange::new("`chrono`", "0.4.38", "0.4.39"));
-        assert_eq!(changes[2], DependabotChange::new("`semver`", "1.0.23", "1.0.24"));
-        assert_eq!(changes[3], DependabotChange::new("`env_logger`", "0.11.5", "0.11.6"));
+        assert_eq!(
+            changes[0],
+            DependabotChange::new("`serde`", "1.0.215", "1.0.216")
+        );
+        assert_eq!(
+            changes[1],
+            DependabotChange::new("`chrono`", "0.4.38", "0.4.39")
+        );
+        assert_eq!(
+            changes[2],
+            DependabotChange::new("`semver`", "1.0.23", "1.0.24")
+        );
+        assert_eq!(
+            changes[3],
+            DependabotChange::new("`env_logger`", "0.11.5", "0.11.6")
+        );
         assert_eq!(changes[4], DependabotChange::new("`zip`", "2.2.1", "2.2.2"));
-        assert_eq!(changes[5], DependabotChange::new("`wasm-bindgen-futures`", "0.4.47", "0.4.49"));
-        assert_eq!(changes[6], DependabotChange::new("`web-sys`", "0.3.74", "0.3.76"));
-        assert_eq!(changes[7], DependabotChange::new("`thiserror`", "2.0.4", "2.0.9"));
-
+        assert_eq!(
+            changes[5],
+            DependabotChange::new("`wasm-bindgen-futures`", "0.4.47", "0.4.49")
+        );
+        assert_eq!(
+            changes[6],
+            DependabotChange::new("`web-sys`", "0.3.74", "0.3.76")
+        );
+        assert_eq!(
+            changes[7],
+            DependabotChange::new("`thiserror`", "2.0.4", "2.0.9")
+        );
 
         let changes_md = format_changes(changes);
         let expect_md = "\
@@ -207,14 +237,24 @@ mod tests {
     fn test_parse_body_1_submodule_short_sha() {
         let changes = parse_changes(DEPENDABOT_BODY_1_SUBMODULE_SHORT_SHA);
         assert_eq!(changes.len(), 1);
-        assert_eq!(changes[0], DependabotChange::new("[some-submodule](https://github.com/updates-org/some-submodule)", "`b0c35f6`", "`c8bd600`"));
+        assert_eq!(
+            changes[0],
+            DependabotChange::new(
+                "[some-submodule](https://github.com/updates-org/some-submodule)",
+                "`b0c35f6`",
+                "`c8bd600`"
+            )
+        );
     }
 
     #[test]
     fn test_parse_body_1_docker_novel_version() {
         let changes = parse_changes(DEPENDABOT_BODY_1_DOCKER_NOVEL_VERSION);
         assert_eq!(changes.len(), 1);
-        assert_eq!(changes[0], DependabotChange::new("ubi9/ubi", "9.4-1214.1726694543", "9.4-1214.1729773476"));
+        assert_eq!(
+            changes[0],
+            DependabotChange::new("ubi9/ubi", "9.4-1214.1726694543", "9.4-1214.1729773476")
+        );
     }
 
     #[test]
@@ -228,7 +268,9 @@ Updates baz from 2024.1.2 to 2025.1.2-rc1
         let changes = parse_body(pr_body);
         assert_eq!(changes.len(), 2);
         assert_eq!(changes[0], DependabotChange::new("foo", "0.1.0a", "0.1.1b"));
-        assert_eq!(changes[1], DependabotChange::new("baz", "2024.1.2", "2025.1.2-rc1"));
+        assert_eq!(
+            changes[1],
+            DependabotChange::new("baz", "2024.1.2", "2025.1.2-rc1")
+        );
     }
-
 }
--- src/test_util/example_dependabot_pr_bodies.rs
+++ src/test_util/example_dependabot_pr_bodies.rs
@@ -7,7 +7,10 @@ macro_rules! dependabot_body {
     };
 }
 
-pub const DEPENDABOT_BODY_2_ACTIONS_SHA_SEMVER: &str = dependabot_body!("2_actions_sha_and_semver.md");
+pub const DEPENDABOT_BODY_2_ACTIONS_SHA_SEMVER: &str =
+    dependabot_body!("2_actions_sha_and_semver.md");
 pub const DEPENDABOT_BODY_7_CRATES_SEMVER: &str = dependabot_body!("7_crates_semver.md");
-pub const DEPENDABOT_BODY_1_SUBMODULE_SHORT_SHA: &str = dependabot_body!("1_submodule_short_sha.md");
-pub const DEPENDABOT_BODY_1_DOCKER_NOVEL_VERSION: &str = dependabot_body!("1_docker_novel_version.md");
+pub const DEPENDABOT_BODY_1_SUBMODULE_SHORT_SHA: &str =
+    dependabot_body!("1_submodule_short_sha.md");
+pub const DEPENDABOT_BODY_1_DOCKER_NOVEL_VERSION: &str =
+    dependabot_body!("1_docker_novel_version.md");
