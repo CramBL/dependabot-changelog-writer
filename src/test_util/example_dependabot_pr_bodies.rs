99b530fd74958307ac2569660da12649eb457c0e
Marc Beck König <mbkj@tutamail.com>
2024-12-30 00:04:34 +0100

    use macro to reduce duplication and add the two new example depandabot pr bodies

--- src/test_util/example_dependabot_pr_bodies.rs
+++ src/test_util/example_dependabot_pr_bodies.rs
@@ -1,6 +1,13 @@
+macro_rules! dependabot_body {
+    ($file:expr) => {
+        concat!(
+            include_str!(concat!("../../test_data/dependabot_pr_bodies/", $file)),
+            include_str!("../../test_data/dependabot_pr_bodies/dependabot_commands_boilerplate.md")
+        )
+    };
+}
 
-pub const DEPENDABOT_BODY_2_ACTIONS_SHA_SEMVER: &str = include_str!("../../test_data/dependabot_pr_bodies/2_actions_sha_and_semver.md");
-
-pub const DEPENDABOT_BODY_7_CRATES_SEMVER: &str = include_str!("../../test_data/dependabot_pr_bodies/7_crates_semver.md");
-
-pub const DEPENDABOT_BODY_1_SUBMODULE_SHORT_SHA: &str = include_str!("../../test_data/dependabot_pr_bodies/1_submodule_short_sha.md");
+pub const DEPENDABOT_BODY_2_ACTIONS_SHA_SEMVER: &str = dependabot_body!("2_actions_sha_and_semver.md");
+pub const DEPENDABOT_BODY_7_CRATES_SEMVER: &str = dependabot_body!("7_crates_semver.md");
+pub const DEPENDABOT_BODY_1_SUBMODULE_SHORT_SHA: &str = dependabot_body!("1_submodule_short_sha.md");
+pub const DEPENDABOT_BODY_1_DOCKER_NOVEL_VERSION: &str = dependabot_body!("1_docker_novel_version.md");
