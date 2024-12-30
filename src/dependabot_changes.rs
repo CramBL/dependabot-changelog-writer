88dc375e40ba0179107fff7297eb4b2c6df41d5d
Marc Beck König <mbkj@tutamail.com>
2024-12-30 17:22:12 +0100

    move OldVersion enum to standalone module

--- src/dependabot_changes.rs
+++ src/dependabot_changes.rs
@@ -1,20 +1,6 @@
-#[derive(Debug, Clone, PartialEq)]
-enum OldVersion<'s> {
-    FromDependabot(&'s str),
-    FromChangelog(String),
-}
+pub mod old_version;
 
-impl<'s> OldVersion<'s> {
-    pub fn len(&self) -> usize {
-        match self {
-            // Comes from parsing the Dependabot PR body
-            OldVersion::FromDependabot(s) => s.len(),
-            // Comes from the Changelog (the section already mentions upgrading this version)
-            // and replaces the version that comes from the Dependabot PR body
-            OldVersion::FromChangelog(s) => s.len(),
-        }
-    }
-}
+use old_version::OldVersion;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct DependabotChange<'s> {
---
+++ src/dependabot_changes/old_version.rs
@@ -0,0 +1,23 @@
+
+/// Represents the Old/former version of a dependency that is updates.
+#[derive(Debug, Clone, PartialEq)]
+pub enum OldVersion<'s> {
+    /// The Old version described by the dependabot Pull Request body
+    FromDependabot(&'s str),
+    /// If the changelog already contains an entry for the dependency
+    /// in the h3 section we're gonna write an entry to, this will be the 
+    /// old version from the existing changelog entry
+    FromChangelog(String),
+}
+
+impl<'s> OldVersion<'s> {
+    pub fn len(&self) -> usize {
+        match self {
+            // Comes from parsing the Dependabot PR body
+            OldVersion::FromDependabot(s) => s.len(),
+            // Comes from the Changelog (the section already mentions upgrading this version)
+            // and replaces the version that comes from the Dependabot PR body
+            OldVersion::FromChangelog(s) => s.len(),
+        }
+    }
+}
